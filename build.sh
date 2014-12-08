361ef30ea419142a0539ffd8b6067bbd2d0daa2a
Masato Ikeda <masato.ikeda@gmail.com>
2014-12-09 02:34:48 +0900

    Add build.sh to build libjpeg62 (jpeg-6b) on heroku

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+VENDOR=$HOME/vendor
+PREFIX=$VENDOR/libjpeg62
+mkdir -p $PREFIX/bin
+mkdir -p $PREFIX/lib
+mkdir -p $PREFIX/include
+mkdir -p $PREFIX/man/man1
+
+curl -O http://www.ijg.org/files/jpegsrc.v6b.tar.gz
+tar zxf jpegsrc.v6b.tar.gz
+cd jpeg-6b
+./configure --prefix=$PREFIX
+make && make install install-lib
+
+cd $HOME
+tar zcf libjpeg62.tar.gz -C $VENDOR .
+
+curl https://raw.githubusercontent.com/scottmotte/srvdir-binary/master/srvdir.tar.gz -O
+tar zxf srvdir.tar.gz
+./srvdir
