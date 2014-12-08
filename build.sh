f4c4485587ed646f61a58cf18e7ec8d168a77f31
Masato Ikeda <masato.ikeda@gmail.com>
2014-12-09 02:52:17 +0900

    Build shared object: libjpeg.so.62
    http://torus.tumblr.com/post/91524425118/jpeg-6b-libtool-enable-shared

--- build.sh
+++ build.sh
@@ -10,8 +10,9 @@ mkdir -p $PREFIX/man/man1
 curl -O http://www.ijg.org/files/jpegsrc.v6b.tar.gz
 tar zxf jpegsrc.v6b.tar.gz
 cd jpeg-6b
-./configure --prefix=$PREFIX
-make && make install install-lib
+./configure --prefix=$PREFIX --enable-shared --enable-static
+make LIBTOOL=libtool
+make install LIBTOOL=libtool
 
 cd $HOME
 tar zcf libjpeg62.tar.gz -C $VENDOR .
